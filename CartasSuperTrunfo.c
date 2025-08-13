808d49b60eaeae115e945bff65be1aef4fd8fe32
AndrePereiiira <andrezinho009@gmail.com>
2025-08-13 18:28:33 +0000

    Desafio: nível mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,6 +5,7 @@ int main() {
     char estado1[8];
     char Codigodacarta1[4];
     char Nomedacidade1[50];
+    char carta1;
     float populacao1; 
     int NumerodepontosTuristicos1;
     float Area1;
@@ -38,6 +39,7 @@ int main() {
 
     float densidadePopulacional1 = populacao1 / Area1;
     float pibPerCapita1 = PIB1 / populacao1;
+    float superPoder1 = (float)populacao1 + Area1 + PIB1 + NumerodepontosTuristicos1 + pibPerCapita1 + densidadePopulacional1;
 
     printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional1);
     printf("PIB per Capita: %f\n", pibPerCapita1);
@@ -46,6 +48,7 @@ int main() {
     char estado2[8];
     char Codigodacarta2[4];
     char Nomedacidade2[50];
+    char carta2;
     float populacao2; 
     int NumerodepontosTuristicos2;
     float area2;
@@ -79,10 +82,20 @@ int main() {
 
     float densidadePopulacional2 = populacao2 / area2;
     float pibPerCapita2 = PIB2 / populacao2;
+    float superPoder2 = (float)populacao2 + area2 + PIB2 + NumerodepontosTuristicos2 + pibPerCapita2 + densidadePopulacional2;
 
+    
     printf("Densidade Populaciona2: %.2f hab/km²\n", densidadePopulacional2);
     printf("PIB per Capita: %f\n", pibPerCapita2); 
-
+    printf("Comparações:\n", (carta1) > (carta2));
+    printf("População: %d\n", populacao1 > populacao2);
+    printf("Área: %d\n", Area1 > area2);
+    printf("PIB: %d\n", PIB1 > PIB2);
+    printf("Número de pontos turísticos: %d\n", NumerodepontosTuristicos1 > NumerodepontosTuristicos2);
+    printf("Densidade Populacional: %d\n", densidadePopulacional1 < densidadePopulacional2);
+    printf("PIB per Capita: %d\n", pibPerCapita1 > pibPerCapita2);
+    printf("Super Poder: %d\n", superPoder1 > superPoder2);
+   
 
 return 0;
 
